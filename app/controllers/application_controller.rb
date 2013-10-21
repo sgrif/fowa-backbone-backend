0b495725965faee912637cc4d02a9f0412c6dc15
Sean Griffin <sean@thoughtbot.com>
2013-10-20 20:22:40 -0600

    Allow all headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def allow_cross_domain_access
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
   end
 
   def options
