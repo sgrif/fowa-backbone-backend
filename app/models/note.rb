57f7e13ec44c3213ac5ac963c9a0a67c122882f6
Sean Griffin <sean@thoughtbot.com>
2013-10-21 09:49:13 -0600

    Validate notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,13 @@ class Note < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
+  validate :ensure_title_or_content
+
+  private
+
+  def ensure_title_or_content
+    unless title.present? || content.present?
+      errors.add(:title, :blank)
+    end
+  end
 end
