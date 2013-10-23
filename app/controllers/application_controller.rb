0aa3347e8e1a67b224b11b39c030dc157a6c8d68
Sean Griffin <sean@thoughtbot.com>
2013-10-23 13:24:38 +0100

    Explicitly allow request methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def allow_cross_domain_access
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE"
     response.headers["Access-Control-Allow-Headers"] = "accept, content-type"
   end
 
